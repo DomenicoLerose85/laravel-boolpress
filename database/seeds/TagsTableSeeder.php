3625a40532568451c717e81f0b7f3266b8fe0689
DomenicoLerose85 <domenicolerose85@libero.it>
2022-06-28 15:18:46 +0200

    tagsTableseeder_done_&_seed_launched

--- database/seeds/TagsTableSeeder.php
+++ database/seeds/TagsTableSeeder.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Database\Seeder;
 
+use Illuminate\Support\Str;
+
+use App\Tag;
+
 class TagsTableSeeder extends Seeder
 {
     /**
@@ -11,6 +15,12 @@ class TagsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $tags = ['Vegetarian', 'Gluten Free', 'Meat', 'Fish'];
+        for ($i = 0; $i < count($tags); $i++) {
+            $newTag = new Tag();
+            $newTag->name = $tags[$i];
+            $newTag->slug = Str::of($newTag->name)->slug('-');
+            $newTag->save();
+        }
     }
 }
